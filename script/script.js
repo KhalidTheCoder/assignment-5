144899d610f3ac2dbaeb5f2f48d9f229e22ea8f9
Khalidahsan007 <khalidasan2407@gmail.com>
2025-03-04 12:54:28 +0600

    java script functionality added

--- script/script.js
+++ script/script.js
@@ -6,6 +6,11 @@ function randomColor() {
   return colors[randomIndex];
 }
 
+function getCurrentTime() {
+  const now = new Date();
+  return now.toLocaleTimeString();
+}
+
 themeBtn.addEventListener("click", function () {
   document.body.style.backgroundColor = randomColor();
 });
@@ -24,3 +29,159 @@ for (const button of buttons) {
     event.target.setAttribute("disabled", "");
   });
 }
+
+let completedTask = parseInt(
+  document.getElementById("task-completed").innerText
+);
+let assignedTask = parseInt(document.getElementById("assigned-task").innerText);
+const firstTitle = document.getElementById("first-title").innerText;
+
+const activityLog = document.getElementById("activity-log");
+
+document.getElementById("mobile-btn").addEventListener("click", function () {
+  completedTask = completedTask + 1;
+  document.getElementById("task-completed").innerText = completedTask;
+
+  assignedTask = assignedTask - 1;
+  document.getElementById("assigned-task").innerText = assignedTask;
+
+  const activityAdd = document.createElement("p");
+  activityAdd.textContent = `You have Complete The Task ${firstTitle} at ${getCurrentTime()}`;
+  activityAdd.classList.add(
+    "text-gray-700",
+    "my-3",
+    "w-[318px]",
+    "h-[68px]",
+    "bg-[#F4F7FF]",
+    "p-2",
+    "rounded-xl"
+  );
+  activityLog.appendChild(activityAdd);
+});
+
+const secondTitle = document.getElementById("second-title").innerText;
+document.getElementById("pay-btn").addEventListener("click", function () {
+  completedTask = completedTask + 1;
+  document.getElementById("task-completed").innerText = completedTask;
+
+  assignedTask = assignedTask - 1;
+  document.getElementById("assigned-task").innerText = assignedTask;
+
+  const activityAdd = document.createElement("p");
+  activityAdd.textContent = `You have Complete The Task ${secondTitle} at ${getCurrentTime()}`;
+  activityAdd.classList.add(
+    "text-gray-700",
+    "my-3",
+    "w-[318px]",
+    "h-[68px]",
+    "bg-[#F4F7FF]",
+    "p-2",
+    "rounded-xl"
+  );
+  activityLog.appendChild(activityAdd);
+});
+
+const thirdTitle = document.getElementById("third-title").innerText;
+document.getElementById("home-btn").addEventListener("click", function () {
+  completedTask = completedTask + 1;
+  document.getElementById("task-completed").innerText = completedTask;
+
+  assignedTask = assignedTask - 1;
+  document.getElementById("assigned-task").innerText = assignedTask;
+
+  const activityAdd = document.createElement("p");
+  activityAdd.textContent = `You have Complete The Task ${thirdTitle} at ${getCurrentTime()}`;
+  activityAdd.classList.add(
+    "text-gray-700",
+    "my-3",
+    "w-[318px]",
+    "h-[68px]",
+    "bg-[#F4F7FF]",
+    "p-2",
+    "rounded-xl"
+  );
+  activityLog.appendChild(activityAdd);
+});
+
+const fourthTitle = document.getElementById("fourth-title").innerText;
+document.getElementById("emoji-btn").addEventListener("click", function () {
+  completedTask = completedTask + 1;
+  document.getElementById("task-completed").innerText = completedTask;
+
+  assignedTask = assignedTask - 1;
+  document.getElementById("assigned-task").innerText = assignedTask;
+
+  const activityAdd = document.createElement("p");
+  activityAdd.textContent = `You have Complete The Task ${fourthTitle} at ${getCurrentTime()}`;
+  activityAdd.classList.add(
+    "text-gray-700",
+    "my-3",
+    "w-[318px]",
+    "h-[68px]",
+    "bg-[#F4F7FF]",
+    "p-2",
+    "rounded-xl"
+  );
+  activityLog.appendChild(activityAdd);
+});
+
+const fifthTitle = document.getElementById("fifth-title").innerText;
+document.getElementById("ai-btn").addEventListener("click", function () {
+  completedTask = completedTask + 1;
+  document.getElementById("task-completed").innerText = completedTask;
+
+  assignedTask = assignedTask - 1;
+  document.getElementById("assigned-task").innerText = assignedTask;
+
+  const activityAdd = document.createElement("p");
+  activityAdd.textContent = `You have Complete The Task ${fifthTitle} at ${getCurrentTime()}`;
+  activityAdd.classList.add(
+    "text-gray-700",
+    "my-3",
+    "w-[318px]",
+    "h-[68px]",
+    "bg-[#F4F7FF]",
+    "p-2",
+    "rounded-xl"
+  );
+  activityLog.appendChild(activityAdd);
+});
+
+const sixthTitle = document.getElementById("sixth-title").innerText;
+document.getElementById("job-btn").addEventListener("click", function () {
+  completedTask = completedTask + 1;
+  document.getElementById("task-completed").innerText = completedTask;
+
+  assignedTask = assignedTask - 1;
+  document.getElementById("assigned-task").innerText = assignedTask;
+
+  const activityAdd = document.createElement("p");
+  activityAdd.textContent = `You have Complete The Task ${sixthTitle} at ${getCurrentTime()}`;
+  activityAdd.classList.add(
+    "text-gray-700",
+    "my-3",
+    "w-[318px]",
+    "h-[68px]",
+    "bg-[#F4F7FF]",
+    "p-2",
+    "rounded-xl"
+  );
+  activityLog.appendChild(activityAdd);
+});
+
+document.getElementById("clear-btn").addEventListener("click", function () {
+  activityLog.replaceChildren();
+});
+
+document.getElementById("date").innerHTML =
+  new Date().toLocaleDateString("en-US", { weekday: "short" }) +
+  " ,<br>" +
+  new Date().toLocaleDateString("en-US", {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  });
+
+document.getElementById("new-window").addEventListener("click", function () {
+  window.open("new.html", "_blank");
+});
